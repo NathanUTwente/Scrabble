59887ed5ed821277d8bf42e79671c27e2fbaf96e
NathanUTwente <95768036+NathanUTwente@users.noreply.github.com>
2022-01-17 13:21:51 +0100

    Stub implementation of Player.java

--- src/Scrabble/Model/PlayerModels/Player.java
+++ src/Scrabble/Model/PlayerModels/Player.java
@@ -1,4 +1,36 @@
 package Scrabble.Model.PlayerModels;
 
-public class Player {
+import Scrabble.Model.BoardModel.Tile;
+
+public abstract class Player {
+    private String name;
+    private Tile[] tileDeck;
+    private int points;
+
+
+    /**
+     * Creates a new player with a given name, an empty tile deck and 0 points
+     * @param name name of player
+     */
+    public Player(String name) {
+        this.name = name;
+        this.tileDeck = new Tile[7];
+        this.points = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    /**
+     * Gives tiles to player to add to their
+     * @param tiles tile array of tiles to give player
+     */
+    public void giveTiles(Tile[] tiles){
+
+    }
 }
