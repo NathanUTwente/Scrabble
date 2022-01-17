e02e58f52c1229daec9206d060522b93c6471e70
NathanUTwente <95768036+NathanUTwente@users.noreply.github.com>
2022-01-17 18:02:49 +0100

    Added MoveChecker.java and started stub implementation

---
+++ src/Utils/MoveChecker.java
@@ -0,0 +1,17 @@
+package Utils;
+
+import Scrabble.Model.BoardModel.Board;
+
+public class MoveChecker {
+    private Board board;
+
+    /**
+     * This method will check if the move is valid and return the result
+     * @param move to be checked
+     * @param board to check the move on
+     * @return true if the move is valid
+     */
+    public boolean checkMove(String[] move, Board board){
+        return false;
+    }
+}
