dc7b7019d3a31d87f5003b9e8c8efb7ea7d42fbe
NathanUTwente <95768036+NathanUTwente@users.noreply.github.com>
2022-01-17 12:45:40 +0100

    Implementation of Square.java

--- src/Scrabble/Model/Square.java
+++ src/Scrabble/Model/Square.java
@@ -1,4 +1,24 @@
 package Scrabble.Model;
 
 public class Square {
+    private Tile tile;
+    private int[] location;
+
+
+    public Square(int[] location) {
+        this.location = location;
+        this.tile = new Tile(Tile.TileType.BLANK, 0);
+    }
+
+    public int[] getLocation() {
+        return location;
+    }
+
+    public Tile getTile() {
+        return tile;
+    }
+
+    public void setTile(Tile tile) {
+        this.tile = tile;
+    }
 }
