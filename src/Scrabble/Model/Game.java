aa5d236d4a6c5b2e2fa999cba5fe7ea65406cbde
NathanUTwente <95768036+NathanUTwente@users.noreply.github.com>
2022-01-17 17:18:42 +0100

    Started Game.java

--- src/Scrabble/Model/Game.java
+++ src/Scrabble/Model/Game.java
@@ -1,4 +1,24 @@
 package Scrabble.Model;
 
+import Scrabble.Model.BoardModel.Board;
+import Scrabble.Model.PlayerModels.Player;
+import Scrabble.View.TextBoardRepresentation;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Game {
+    private Player[] players;
+
+    private Board board;
+
+    private Map<Player, Integer> scores;
+
+
+    public Game() {
+    }
+
+    public void start(){}
+
 }
