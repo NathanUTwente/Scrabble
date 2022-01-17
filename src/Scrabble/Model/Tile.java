8e456f26a552a202226666b3b36e2c66b44dc9a5
NathanUTwente <95768036+NathanUTwente@users.noreply.github.com>
2022-01-17 12:34:31 +0100

    Implementation of Tile.java

--- src/Scrabble/Model/Tile.java
+++ src/Scrabble/Model/Tile.java
@@ -1,4 +1,23 @@
 package Scrabble.Model;
 
 public class Tile {
+    private enum TileType{
+        BLANK, AA, BB, CC, DD, EE, FF, GG, HH, II, JJ, KK, LL, MM, NN, OO, PP, QQ, RR, SS, TT, UU, VV, WW, XX, YY, ZZ
+    }
+
+    private TileType tileType;
+    private int points;
+
+    public Tile(TileType tileType, int points) {
+        this.tileType = tileType;
+        this.points = points;
+    }
+
+    public TileType getTileType() {
+        return tileType;
+    }
+
+    public int getPoints() {
+        return points;
+    }
 }
