f30b77c88b178c646d05a20059b3b437891fcff5
NathanUTwente <95768036+NathanUTwente@users.noreply.github.com>
2022-01-17 17:49:25 +0100

    Stub implementation of the TextBoardRepresentation.java

--- src/Scrabble/View/TextBoardRepresentation.java
+++ src/Scrabble/View/TextBoardRepresentation.java
@@ -1,8 +1,40 @@
 package Scrabble.View;
 
+import Scrabble.Model.BoardModel.Board;
+import Scrabble.Model.Game;
+import Scrabble.Model.PlayerModels.Player;
+
 public class TextBoardRepresentation {
 
-    public void update(){
+    /**
+     * Method updates the TUI with the current board state
+     * @param board board to be displayed
+     */
+    public void update(Board board){
+    }
+
+    /**
+     * Gets input from specified player about move
+     * @param player to get move from
+     * @return the move instructions parsed into a string array
+     */
+    public String[] getMove(Player player){
+        return null;
+    }
+
+    /**
+     * Updates and displays the tile deck for the current player
+     */
+    public void updatePlayerDeck(){
+    }
+
+    /**
+     * Displays the results at the end of the given game
+     * @requires game.isOver == true
+     * @param game the game to d
+     */
+    public void displayResults(Game game){
 
     }
+
 }
