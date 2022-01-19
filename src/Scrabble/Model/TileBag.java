6d0226230f6367a0c6952a2c2ca1726643cbcc72
NathanUTwente <95768036+NathanUTwente@users.noreply.github.com>
2022-01-19 12:09:18 +0100

    Fixed TileBag

--- src/Scrabble/Model/TileBag.java
+++ src/Scrabble/Model/TileBag.java
@@ -9,7 +9,7 @@ import java.util.Random;
 
 public class TileBag {
     public ArrayList<Tile> tileList;
-    final private ArrayList<Tile.TileType> letters;
+    final private ArrayList<Tile> tileBag;
 
     public static void main(String[] args) {
         new TileBag();
@@ -47,68 +47,68 @@ public class TileBag {
         tileList.add(new Tile(Tile.TileType.Z, 10));
         tileList.add(new Tile(Tile.TileType.BLANK, 0));
         
-        letters = new ArrayList<>();
+        tileBag = new ArrayList<>();
 
         for (int i = 0; i < 12; i++) {
-            letters.add(Tile.TileType.E);
+            tileBag.add(new Tile(Tile.TileType.E, 1));
         }
 
         for (int i = 0; i < 9; i++) {
-            letters.add(Tile.TileType.A);
-            letters.add(Tile.TileType.I);
+            tileBag.add(new Tile(Tile.TileType.A, 1));
+            tileBag.add(new Tile(Tile.TileType.I, 1));
         }
 
         for (int i = 0; i < 8; i++) {
-            letters.add(Tile.TileType.O);
+            tileBag.add(new Tile(Tile.TileType.O, 1));
         }
 
         for (int i = 0; i < 6; i++) {
-            letters.add(Tile.TileType.N);
-            letters.add(Tile.TileType.R);
-            letters.add(Tile.TileType.T);
+            tileBag.add(new Tile(Tile.TileType.N, 1));
+            tileBag.add(new Tile(Tile.TileType.R, 1));
+            tileBag.add(new Tile(Tile.TileType.T, 1));
         }
 
         for (int i = 0; i < 4; i++) {
-            letters.add(Tile.TileType.L);
-            letters.add(Tile.TileType.S);
-            letters.add(Tile.TileType.U);
-            letters.add(Tile.TileType.D);
+            tileBag.add(new Tile(Tile.TileType.L, 1));
+            tileBag.add(new Tile(Tile.TileType.S, 1));
+            tileBag.add(new Tile(Tile.TileType.U, 1));
+            tileBag.add(new Tile(Tile.TileType.D, 1));
         }
 
         for (int i = 0; i < 3; i++) {
-            letters.add(Tile.TileType.G);
+            tileBag.add(new Tile(Tile.TileType.G, 2));
         }
 
         for (int i = 0; i < 2; i++) {
-            letters.add(Tile.TileType.B);
-            letters.add(Tile.TileType.C);
-            letters.add(Tile.TileType.M);
-            letters.add(Tile.TileType.P);
-            letters.add(Tile.TileType.F);
-            letters.add(Tile.TileType.H);
-            letters.add(Tile.TileType.V);
-            letters.add(Tile.TileType.W);
-            letters.add(Tile.TileType.Y);
-            letters.add(Tile.TileType.BLANK);
+            tileBag.add(new Tile(Tile.TileType.B, 3));
+            tileBag.add(new Tile(Tile.TileType.C, 3));
+            tileBag.add(new Tile(Tile.TileType.M, 3));
+            tileBag.add(new Tile(Tile.TileType.P, 3));
+            tileBag.add(new Tile(Tile.TileType.F, 4));
+            tileBag.add(new Tile(Tile.TileType.H, 4));
+            tileBag.add(new Tile(Tile.TileType.V, 4));
+            tileBag.add(new Tile(Tile.TileType.W, 4));
+            tileBag.add(new Tile(Tile.TileType.Y, 4));
+            tileBag.add(new Tile(Tile.TileType.BLANK, 0));
+            tileBag.add(new Tile(Tile.TileType.K, 5));
+            tileBag.add(new Tile(Tile.TileType.J, 8));
         }
 
-        letters.add(Tile.TileType.K);
-        letters.add(Tile.TileType.J);
-        letters.add(Tile.TileType.X);
-        letters.add(Tile.TileType.Q);
-        letters.add(Tile.TileType.Z);
+        tileBag.add(new Tile(Tile.TileType.X, 8));
+        tileBag.add(new Tile(Tile.TileType.Q, 10));
+        tileBag.add(new Tile(Tile.TileType.Z, 10));
     }
 
     public int tilesLeftInBag(){
-        return letters.size();
+        return tileBag.size();
     }
 
-    public Tile.TileType getTileOutOfBag(){
+    public Tile getTileOutOfBag(){
         if (tilesLeftInBag() > 0) {
-            int max = letters.size() - 1;
+            int max = tileBag.size() - 1;
             Random random = new Random();
             int randomTileIndex = random.nextInt(max + 1);
-            return letters.remove(randomTileIndex);
+            return tileBag.remove(randomTileIndex);
         } else {
             return null;
         }
@@ -186,14 +186,13 @@ public class TileBag {
         return 0;
     }
 
-    public ArrayList<Tile.TileType> givePlayerTile(Player player){
-        ArrayList<Tile.TileType> newTile = new ArrayList<>();
+    public ArrayList<Tile> getTilesForPlayer(Player player){
+        ArrayList<Tile> newTile = new ArrayList<>();
         int needed = player.EmptySpotsInDeck();
         for (int i = 0; i < needed; i++) {
             newTile.add(getTileOutOfBag());
-            return newTile;
         }
-        return null;
+        return newTile;
     }
 
 
