60de45814ef0b7f6c0b5227250cedaf495187dff
NathanUTwente <95768036+NathanUTwente@users.noreply.github.com>
2022-02-21 20:40:01 +0100

    LobbyHandler javadoc

--- src/NetworkScrabble/Network/LobbyHandler.java
+++ src/NetworkScrabble/Network/LobbyHandler.java
@@ -9,13 +9,21 @@ public class LobbyHandler {
     ArrayList<ScrabbleClientHandler> clients = new ArrayList<>();
     ArrayList<String> names = new ArrayList<>();
 
-
+    /**
+     * Adds client to lobby
+     * @param clientHandler client to add to lobby
+     */
     public void addClientToLobby(ScrabbleClientHandler clientHandler){
         clients.add(clientHandler);
         names.add(clientHandler.getClientName());
     }
 
 
+    /**
+     * Tells each player who is in the lobby by giving it to each handler
+     * Also asks each player if all players are listed in the lobby
+     * @return true if all players are said to be present, false if not
+     */
     public boolean isAllPlayers() {
         String nameList = "";
         for (String name : names){
