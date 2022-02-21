6f7054ef9710a42f9085a65f8375d95401bf7434
NathanUTwente <95768036+NathanUTwente@users.noreply.github.com>
2022-02-21 20:59:28 +0100

    javadoc

--- src/NetworkScrabble/Network/ScrabbleServer.java
+++ src/NetworkScrabble/Network/ScrabbleServer.java
@@ -23,20 +23,16 @@ public class ScrabbleServer {
     ServerSocket listener;
     ArrayList<ScrabbleClientHandler> clients = new ArrayList<>();
     Socket connection;
-    CountDownLatch countDownLatch;
     GameMaster gameMaster;
     HashMap<String, ScrabbleClientHandler> nameHandlers = new HashMap<>();
     HashMap<String, Player> namePlayers = new HashMap<>();
-//    int players;
     LobbyHandler lobbyHandler;
     ChatManager chatManager;
 
-
+    /**
+     * Starts the server and tells it to do each step
+     */
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(in);
-//        System.out.println("How many players in game?");
-//        int players = scanner.nextInt();
-//        scanner.close();
         ScrabbleServer scrabbleServer = new ScrabbleServer();
         scrabbleServer.setUpServer();
         scrabbleServer.setUpChat(DEFAULT_CHAT_PORT);
@@ -44,6 +40,11 @@ public class ScrabbleServer {
         scrabbleServer.runGame();
     }
 
+    /**
+     * Tells clients to use a specified port for the chat and sets up the chat
+     * Creates a handler on a new thread for each
+     * @param port port that the chat should use to communicate
+     */
     public void setUpChat(int port){
         for (ScrabbleClientHandler scrabbleClientHandler : clients){
             scrabbleClientHandler.sendChatInstructions(port);
@@ -53,11 +54,17 @@ public class ScrabbleServer {
         chat.start();
     }
 
+    /**
+     * Creates server object and creates lobby handler
+     * @ensures lobbyHandler != null
+     */
     public ScrabbleServer() {
-//        this.players = players;
         lobbyHandler = new LobbyHandler();
     }
 
+    /**
+     * Runs game until game is over
+     */
     public void runGame(){
         while (!gameMaster.isGameOver()){
             runTurn();
@@ -66,6 +73,12 @@ public class ScrabbleServer {
         broadcastGameOver();
 
     }
+
+    /**
+     * Sets up the server
+     * Starts a server socket and listens for clients, creating a handler and a thread for each one, then putting them in the lobby
+     * @ensures clients > 0 && nameHandlers > 0
+     */
     public void setUpServer() {
         boolean ready = false;
         try {
@@ -114,6 +127,10 @@ public class ScrabbleServer {
 
     }
 
+    /**
+     * Asks each handler to send ready request to clients and returns result
+     * @return true if all clients are ready, false if not
+     */
     public boolean clientsReady(){
         boolean result = true;
         for (ScrabbleClientHandler client : clients){
@@ -129,6 +146,11 @@ public class ScrabbleServer {
         return result;
     }
 
+    /**
+     * Sets up the game, tells handlers names of all players and to send them out
+     * Creates a GameMaster and gives it a list of players to start a game and sends out the tile decks of each player to the client through the handlers
+     * @ensures gameMaster != null
+     */
     public void setUpGame(){
         sendPlayerList();
         System.out.println("Sent Out Names");
@@ -149,12 +171,24 @@ public class ScrabbleServer {
         }
     }
 
+    /**
+     * Tells all clients to broadcast game over
+     */
     public void broadcastGameOver(){
         for (ScrabbleClientHandler clientHandler : clients){
             clientHandler.broadcastGameOver();
         }
     }
 
+    /**
+     * Runs each turn
+     * Tells handlers to broadcast turn
+     * Gets the move of the player
+     * Checks the move and calculates points
+     * Adds this to the game
+     * Sends the confirmation to players
+     * Sends new tiles to player who played
+     */
     public void runTurn(){
         Player currentPlayer = gameMaster.getCurrentPlayer();
         broadcastTurn(currentPlayer);
@@ -177,6 +211,10 @@ public class ScrabbleServer {
         }
     }
 
+    /**
+     * Broadcasts to all players that a player skipped their turn
+     * @param player the player that skipped
+     */
     public void broadcastPass(Player player){
         for (ScrabbleClientHandler clientHandler : clients){
             clientHandler.broadcastPass(player.getName());
@@ -184,24 +222,42 @@ public class ScrabbleServer {
     }
 
 
+    /**
+     * Broadcasts to all players that the last move was invalid and the player lost their move
+     */
     public void sendInvalidWord(){
         for (ScrabbleClientHandler clientHandler : clients){
             clientHandler.sendInvalidMove();
         }
     }
 
+    /**
+     * Sends to all players the last confirmed move
+     * @param move the confirmed move
+     * @param earnedPoints the points the move earned
+     * @param currentPlayer the player who made the move
+     */
     public void sendEndOfTurn(String[] move, int earnedPoints, Player currentPlayer){
         for (ScrabbleClientHandler clientHandler : clients){
             clientHandler.sendMoveConfirm(move, earnedPoints, currentPlayer);
         }
     }
 
+    /**
+     * Broadcasts to all players whose turn it is
+     * @param player the current player
+     */
     public void broadcastTurn(Player player){
         for (ScrabbleClientHandler clientHandler : clients){
             clientHandler.broadcastTurn(player.getName());
         }
     }
 
+    /**
+     * Asks the player for their move
+     * @param player the player whose turn it is
+     * @return the move the player sends back
+     */
     public String[] getCurrentMove(Player player){
         try {
             return nameHandlers.get(player.getName()).getTurnMove();
@@ -211,6 +267,13 @@ public class ScrabbleServer {
         return null;
     }
 
+    /**
+     * Checks what type of move is played and whether it is valid
+     * @param move the move
+     * @return validity of move/type
+     * @throws InvalidNetworkMoveException if the move is invalid
+     * @throws TileBagEmptyException if the tiles cannot be swapped
+     */
     public int processMove(String[] move) throws InvalidNetworkMoveException, TileBagEmptyException {
             if (move[0].equals("PASS")) {
                 if (move.length == 2) {
@@ -224,6 +287,11 @@ public class ScrabbleServer {
             }
     }
 
+    /**
+     *  Sends new tiles to the player who just made a move
+     * @param currentPlayer
+     * @param move
+     */
     public void sendNewTiles(Player currentPlayer, String[] move){
         ArrayList<Tile> tiles = gameMaster.getNewTiles(currentPlayer, move);
         String[] tileStrings = new String[tiles.size()];
@@ -234,6 +302,11 @@ public class ScrabbleServer {
 
     }
 
+    /**
+     * Sends a specified number of tiles to the specified player
+     * @param currentPlayer
+     * @param number
+     */
     public void sendNewTiles(Player currentPlayer, int number){
         String[] tileStrings = new String[number];
         for (int i = 0; i < number; i++){
@@ -242,6 +315,10 @@ public class ScrabbleServer {
         nameHandlers.get(currentPlayer.getName()).sendTiles(tileStrings);
 
     }
+
+    /**
+     * Broadcasts the player list ot each player
+     */
     public void sendPlayerList(){
         String[] playerList = new String[clients.size()];
         for (int i = 0; i < clients.size(); i++) {
