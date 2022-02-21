b4735e7a07a5bf77ea7684490b31aeaa9d837f76
NathanUTwente <95768036+NathanUTwente@users.noreply.github.com>
2022-02-21 22:22:31 +0100

    Removed unused shit

--- src/Scrabble/Tests/GameTest.java
+++ src/Scrabble/Tests/GameTest.java
@@ -1,12 +1,10 @@
 package Scrabble.Tests;
 
-import Scrabble.Controller.GameMaster;
 import Scrabble.Model.PlayerModels.HumanPlayer;
 import org.junit.jupiter.api.Test;
 import Scrabble.Model.PlayerModels.Player;
 import Scrabble.Model.Game;
 
-import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,7 +13,6 @@ class GameTest {
 
     @Test
     void updatePointsTest() {
-        //scores.put(player, scores.get(player) + points);
         Player[] players = new Player[]{new HumanPlayer("Lejla"), new HumanPlayer("Nathan")};
         Game game = new Game(players);
         Player playerOne = players[0];
