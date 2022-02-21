7062e9e6d75d7c1d8a357f9e9963c9ef13f6c0f7
NathanUTwente <95768036+NathanUTwente@users.noreply.github.com>
2022-02-21 21:28:46 +0100

    javadoc

--- src/NetworkScrabble/Network/ScrabbleClientHandler.java
+++ src/NetworkScrabble/Network/ScrabbleClientHandler.java
@@ -21,6 +21,11 @@ public class ScrabbleClientHandler implements Runnable{
 
     Scanner userInput;
 
+    /**
+     * Sets up the handler on the given socket
+     * @param connection
+     * @throws IOException
+     */
     public ScrabbleClientHandler(Socket connection) throws IOException {
         this.connection = connection;
         this.in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
@@ -28,6 +33,10 @@ public class ScrabbleClientHandler implements Runnable{
         userInput = new Scanner(System.in);
     }
 
+    /**
+     * Waits to here from the client then sends a handshake message
+     * @throws IOException
+     */
     public void doHandShake() throws IOException {
         String messageIn = in.readLine();
         if ((messageIn.split(ProtocolMessages.SEPARATOR)[0].equals(ProtocolMessages.HELLO)) ){
@@ -41,10 +50,9 @@ public class ScrabbleClientHandler implements Runnable{
 
     }
 
-    public void setCountDownLatch(CountDownLatch countDownLatch) {
-        this.countDownLatch = countDownLatch;
-    }
-
+    /**
+     * Does the handshake and then notifies the server
+     */
     @Override
     public void run() {
         synchronized (this) {
@@ -54,13 +62,18 @@ public class ScrabbleClientHandler implements Runnable{
                 wake = true;
                 this.notifyAll();
 
-//            countDownLatch.countDown();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
+    /**
+     * Sends out a ready message with the list of players connected and waits for a confirmation that all players are connected
+     * @param nameList
+     * @return whether the client says all players are connected
+     * @throws IOException
+     */
     public boolean allPlayersConnected(String nameList) throws IOException {
         out.println(ProtocolMessages.SERVERREADY + ProtocolMessages.SEPARATOR + nameList);
         out.flush();
@@ -69,6 +82,11 @@ public class ScrabbleClientHandler implements Runnable{
         return messageIn.equals(ProtocolMessages.CLIENTREADY);
     }
 
+    /**
+     * Asks client if ready to play
+     * @return true if client is ready
+     * @throws IOException
+     */
     public boolean getClientReady() throws IOException {
         out.println(ProtocolMessages.SERVERREADY);
         out.flush();
@@ -78,11 +96,18 @@ public class ScrabbleClientHandler implements Runnable{
     }
 
 
-
+    /**
+     * Returns the name of the client this handler is responsible for
+     * @return the name
+     */
     public String getClientName() {
         return clientName;
     }
 
+    /**
+     * Sends the list of players to the client at the start of the game
+     * @param playerNames
+     */
     public void sendPlayerList(String[] playerNames){
         String messageOut = ProtocolMessages.HELLO + ProtocolMessages.SEPARATOR;
         for (String name : playerNames){
@@ -92,6 +117,10 @@ public class ScrabbleClientHandler implements Runnable{
         out.flush();
     }
 
+    /**
+     * Sends the given tiles to the player
+     * @param tiles tiles to give
+     */
     public void sendTiles(String[] tiles){
         String messageOut = ProtocolMessages.TILES + ProtocolMessages.SEPARATOR;
         for (String tile : tiles){
@@ -101,6 +130,10 @@ public class ScrabbleClientHandler implements Runnable{
         out.flush();
     }
 
+    /**
+     * Sends the name of the current players turn
+     * @param name of player whose turn it is
+     */
     public void broadcastTurn(String name){
         String messageOut = ProtocolMessages.TURN + ProtocolMessages.SEPARATOR;
         messageOut += name;
@@ -108,6 +141,10 @@ public class ScrabbleClientHandler implements Runnable{
         out.flush();
     }
 
+    /**
+     * Sends the name of the player who passed their turn
+     * @param name of the player who passed
+     */
     public void broadcastPass(String name){
         String messageOut = ProtocolMessages.PASS + ProtocolMessages.SEPARATOR;
         messageOut += name;
@@ -115,6 +152,11 @@ public class ScrabbleClientHandler implements Runnable{
         out.flush();
     }
 
+    /**
+     * Gets the move the player sends to the server
+     * @return the move of the player
+     * @throws IOException
+     */
     public String[] getTurnMove() throws IOException {
         String[] move = new String[3];
         String messageIn = in.readLine();
@@ -131,6 +173,12 @@ public class ScrabbleClientHandler implements Runnable{
         return move;
     }
 
+    /**
+     * Sends out a confirmed move to the player
+     * @param move the move confirmed
+     * @param earnedPoints the points to move earned
+     * @param currentPlayer the player who made the move
+     */
     public void sendMoveConfirm(String[] move, int earnedPoints, Player currentPlayer){
         String messageOut = ProtocolMessages.MOVE + ProtocolMessages.SEPARATOR + currentPlayer.getName() + ProtocolMessages.SEPARATOR;
         for (String part : move){
@@ -141,22 +189,36 @@ public class ScrabbleClientHandler implements Runnable{
         out.flush();
     }
 
+    /**
+     * Sends out the invalid move message
+     */
     public void sendInvalidMove(){
         String messageOut = ProtocolMessages.ERROR+ ProtocolMessages.SEPARATOR + ProtocolMessages.INVALID_MOVE;
         out.println(messageOut);
         out.flush();
     }
 
+    /**
+     * Sends out that the game os over
+     */
     public void broadcastGameOver(){
         String messageOut = ProtocolMessages.GAMEOVER;
         out.println(messageOut);
         out.flush();
     }
 
+    /**
+     * Returns whether wake is needed
+     * @return boolean if wake is needed
+     */
     public boolean wakeNeeded() {
         return wake;
     }
 
+    /**
+     * Sends out the port to be used by the chat
+     * @param port
+     */
     public void sendChatInstructions(int port){
         String messageOut = ProtocolMessages.CHAT_FLAG + ProtocolMessages.SEPARATOR + port;
         out.println(messageOut);
